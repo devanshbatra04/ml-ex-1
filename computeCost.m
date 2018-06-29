4a205cb9a22e8f77c294f8a21102c418dceba67c
Devansh Batra <devanshbatra04@gmail.com>
2018-06-30 02:46:41 +0530

    implemented gradient descent

--- computeCost.m
+++ computeCost.m
@@ -14,7 +14,10 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+temp = X * theta - y;
+tnew = temp .^ 2;
+tnew = sum(tnew);
+J = tnew / (2 * m);
 
 
 % =========================================================================
--- gradientDescent.m
+++ gradientDescent.m
@@ -16,7 +16,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    temp = X * theta - y;
+    tempnew = X' * temp;
+    sumT = sum(tempnew);
+    t = tempnew * alpha / m;
+    theta = theta - t;
+    
 
 
 
